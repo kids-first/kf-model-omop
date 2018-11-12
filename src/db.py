99eead8cf768081c1666c96d9c02514e71fbecd9
Natasha Singh <dukedesi22@gmail.com>
2018-11-12 15:51:23 -0500

    :sparkles: Add drop all tables method

--- src/db.py
+++ src/db.py
@@ -161,3 +161,58 @@ def erd(config=None, filepath=None):
     render_er(config.SQLALCHEMY_DATABASE_URI, filepath)
 
     print(f'Entity relationship diagram generated: {filepath}')
+
+
+def drop_tables(config=None, engine=None):
+    """
+    Drop all tables
+    """
+    config = config or config_dict.get(mode)
+
+    from sqlalchemy.engine import reflection
+    from sqlalchemy.schema import (
+        MetaData,
+        Table,
+        DropTable,
+        ForeignKeyConstraint,
+        DropConstraint,
+    )
+
+    engine = engine or create_engine(config.SQLALCHEMY_DATABASE_URI)
+
+    conn = engine.connect()
+
+    # the transaction only applies if the DB supports
+    # transactional DDL, i.e. Postgresql, MS SQL Server
+    trans = conn.begin()
+
+    inspector = reflection.Inspector.from_engine(engine)
+
+    # gather all data first before dropping anything.
+    # some DBs lock after things have been dropped in
+    # a transaction.
+
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(
+                ForeignKeyConstraint((), (), name=fk['name'])
+            )
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        conn.execute(DropConstraint(fkc))
+
+    for table in tbs:
+        conn.execute(DropTable(table))
+
+    trans.commit()
