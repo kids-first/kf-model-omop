e374c440a494b8dc3d0ab796bf4842c95a44024d
Natasha Singh <dukedesi22@gmail.com>
2018-11-13 12:11:10 -0500

    :sparkles: Add CLI cmd to generate data loader template

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from shutil import copyfile
+
 import click
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -54,7 +58,10 @@ def drop_db():
 
 @click.command()
 @click.option('-o', '--output_filepath',
-              type=click.Path(file_okay=True, dir_okay=False))
+              default=os.path.join(os.getcwd(), 'erd.png'),
+              show_default=True,
+              type=click.Path(file_okay=True, dir_okay=False),
+              help='The location of the generated ERD file')
 def erd(output_filepath):
     """
     Generate an entity relationship diagram from the OMOP database
@@ -64,6 +71,34 @@ def erd(output_filepath):
     erd(filepath=output_filepath)
 
 
+@click.command('code-template')
+@click.option('-o', '--output_dir',
+              default=os.getcwd(),
+              show_default=True,
+              type=click.Path(file_okay=False, dir_okay=True),
+              help='The directory where the py file will be generated')
+def generate_code_template(output_dir):
+    """
+    Generate a .py file with template code for connecting to the DB and
+    loading data in via the ORM models.
+    """
+    fname = 'loader.py'
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    source_path = os.path.join(root_dir, 'factory', 'code_template.py')
+    dest_path = os.path.join(output_dir, fname)
+
+    if os.path.isfile(dest_path):
+        print(f'Error {dest_path} already exists! '
+              'Aborting to avoid overwrite!')
+        sys.exit()
+    else:
+        copyfile(source_path, dest_path)
+
+        print('Generated a template Python module to load data into db via '
+              f'OMOP SQLAlchemy models. \nLocated at {dest_path}')
+
+
 cli.add_command(create_omop)
 cli.add_command(drop_db)
 cli.add_command(erd)
+cli.add_command(generate_code_template)
