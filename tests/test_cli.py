6b687f0bc58454ec0dae635da981b688407e9422
Natasha Singh <dukedesi22@gmail.com>
2018-11-13 12:12:04 -0500

    :white_check_mark: Test CLI cmd code-template

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,16 @@
-
+import os
 from click.testing import CliRunner
 
 import cli
 from config import APP_CONFIG_ENV_VAR
+
+env = {APP_CONFIG_ENV_VAR: 'testing'}
 
 
 def test_create_omop():
     """
     Test create omop db
     """
-    env = {APP_CONFIG_ENV_VAR: 'testing'}
     runner = CliRunner()
     result = runner.invoke(cli.create_omop, [], env=env)
 
@@ -23,3 +24,35 @@ def test_create_omop():
     inspector = inspect(engine)
 
     assert len(inspector.get_table_names()) == 37
+
+
+def test_code_template(tmpdir):
+    """
+    Test cli command code-template
+    """
+    # Create temp file
+    fn = tmpdir.mkdir("data")
+    expected_file = os.path.join(str(fn), 'loader.py')
+
+    # Gen code template
+    runner = CliRunner()
+    result = runner.invoke(cli.generate_code_template,
+                           ['-o', str(fn)], env=env)
+
+    assert result.exit_code == 0
+    assert os.path.isfile(expected_file)
+
+    # Update file
+    with open(expected_file, 'w') as f:
+        f.write('print("hello world!")')
+
+    # Try to generate file again
+    result = runner.invoke(cli.generate_code_template,
+                           ['-o', str(fn)], env=env)
+
+    assert 'already exists' in result.stdout
+    assert result.exit_code == 0
+
+    # Check file
+    with open(expected_file, 'r') as f:
+        assert 'hello world!' in f.read()
